ff4c8b9373648b4c4ed6a8ae07c1a5567c75cfb7
Ethan Nelson <ethan.charles@gmail.com>
2016-09-05 09:37:29 -0700

    added comments to tests to clarify experiment

--- ComplexModelPersistence.Tests/Tests.cs
+++ ComplexModelPersistence.Tests/Tests.cs
@@ -11,17 +11,27 @@ namespace ComplexModelPersistence.Tests
 {
     public class Tests : IClassFixture<DbSetup>
     {
+        /// <summary>
+        /// Generate a random set of input models and execute a merge stored procedure
+        /// Demonstrate that, at least for inserts, data is persited correctly.
+        /// 
+        /// Database internal storage should cluster and relate across integers.
+        /// GUID key relations should remain logically corerct.
+        /// </summary>
+        /// <param name="input"></param>
         [Theory, AutoData]
         public void EndToEndTest(IEnumerable<ComplexModel> input)
         {
             using (var conn = new SqlConnection(Config.ConnectionString))
             {
+                // setup a parameter matching mastermodellist table type
                 var masters = input.Select(x => new
                 {
                     masterid = x.MasterId,
                     mastervalue = x.MasterValue,
                     masteralternatevalue = x.MasterAlternateValue
                 }).AsTableValuedParameter("mastermodellist", new[] { "masterid", "mastervalue", "masteralternatevalue" });
+                // setup a parameter matching innermodellist table type (add master key to relation)
                 var inners =
                     (from a in input
                      from b in a.InnerModels
@@ -34,30 +44,35 @@ namespace ComplexModelPersistence.Tests
                      });
                 var innersDto = inners.AsTableValuedParameter("innermodellist", new[] { "innerid", "relationid", "innervalue", "innerkey" });
 
+                // execute the mergemodels sproc with parameters
                 conn.Execute("mergemodels", new
                 {
                     masters,
                     inners = innersDto
                 }, commandType: CommandType.StoredProcedure);
 
+                // get actaul master values
                 var mastervalues =
                     conn.Query(
                             "select masterid, mastervalue, masteralternatevalue from mastertable order by masterid")
                         .OrderBy(x => x.masterid)
                         .ToArray();
 
+                // get actual inner values
                 var innervalues =
                     conn.Query(
                             "select innerid, innervalue, innerkey from innertable order by innerid")
                         .OrderBy(x => x.innerid)
                         .ToArray();
 
+                // get actual relations
                 var relations =
                     conn.Query(
                             "select innerid, masterid from innertable a join mastertable b on a.masteruid = b.uid")
                         .OrderBy(x => x.innerid)
                         .ToArray();
 
+                // verify master values
                 input
                     .OrderBy(x => x.MasterId)
                     .Iter((x, y) =>
@@ -66,6 +81,8 @@ namespace ComplexModelPersistence.Tests
                         Assert.Equal(mastervalues[y].mastervalue, x.MasterValue);
                         Assert.Equal(mastervalues[y].masteralternatevalue, x.MasterAlternateValue);
                     });
+
+                // verify inner values
                 input
                     .SelectMany(x => x.InnerModels)
                     .OrderBy(x => x.InnerId)
@@ -75,6 +92,8 @@ namespace ComplexModelPersistence.Tests
                         Assert.Equal(innervalues[y].innerkey, x.InnerKey);
                         Assert.Equal(innervalues[y].innervalue, x.InnerValue);
                     });
+
+                // verify relations are correct
                 inners
                     .OrderBy(x => x.innerid)
                     .Iter((x, y) =>
